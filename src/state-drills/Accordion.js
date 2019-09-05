7cfe9c2cca98ccd0b3679eed99605c66083970f3
Phoebe Law <phoebebasilio@gmail.com>
2019-09-05 16:37:36 -0700

    Accordion app functional! On second click of the same button, it 'collapses' the accordion (removes the <p> text)

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -9,7 +9,7 @@ class Accordion extends React.Component {
     activeSectionIndex: null
   }
 
-  //moved this outside of the renderAccordionHtml function to be multiline, so that we can tell it what to do if we click on a button that already has a <p> open
+  // moved this outside of the renderAccordionHtml function so that it can be multiline. This is so we can provide the conditional (that it should reset activeSectionIndex to null so that when the conditional within the renderAccordionHtml runs (and the page rerenders) they will remove the <p> from the DOM) 
   handleButtonClicked = (index) => {
     if (index === this.state.activeSectionIndex) {
       this.setState({
@@ -22,27 +22,29 @@ class Accordion extends React.Component {
     }
   }
 
-  //moved this outside the render function because we needed a multiline function to accomodate the <p> element. I can't think of a way to insert a p element within the same <li> div as the button other than adding it within the function
-  renderAccordionHtml = (section, index, activeSectionIndex) => {
+  //moved this outside the render function because we needed a multiline function to accomodate the <p> element. I can't think of a way to optionally insert a <p> element within the same <li>element as the button other than adding it within the function
+  renderAccordionHtml = (section, index) => {
     return (
     <li>
-      <button key={index} onClick={this.handleButtonClicked(index)}>
+      {/* I think this is maybe part of what they were talking about in the second workshop today... if we omit the () => portion from on-click, it enters in an infinite loop because it calls handleButtonClicked, which rerenders the page, which puts button on again which reruns handleButtonClicked... etc. We can't just put this.handleButtonClicked either because we need to pass the index in   */}
+      <button key={index} onClick={() => this.handleButtonClicked(index)}>
         {section.title}
       </button>
-      {(activeSectionIndex === index) && (<p class='section-content'>{section.content}</p>)}
+      {(this.state.activeSectionIndex === index) && (<p class='section-content'>{section.content}</p>)}
     </li>
     )
   }  
 
   render() {
     const sections = this.props.sections
+    //Just left this here so you know what happened to it in case you compare it to old code 
     // const buttons = this.props.sections.map((section, index) => (
     //   <button key={index}> {section.title} </button>
     // ));
     return  (
       <ul>
         {sections.map((section, index) => {
-          return this.renderAccordionHtml(section, index, this.state.activeSectionIndex)
+          return this.renderAccordionHtml(section, index)
         })}
       </ul>
     )
