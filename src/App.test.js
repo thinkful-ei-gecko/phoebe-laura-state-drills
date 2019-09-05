3fb0a1e66128f2d938402864f840bff4823baefe
laura <laura.j.elias@gmail.com>
2019-09-05 13:02:07 -0500

    with Phoebe for state drills

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//TO RUN Jest type: npm test
+//to run tests again: a
+//to run only failed tests again: f
+//to stop "watch" mode: q
+
+
+
+//this is the test case
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
