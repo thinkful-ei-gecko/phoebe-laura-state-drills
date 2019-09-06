28354eec1bde71b8df62727e4872875e55a06703
laura <laura.j.elias@gmail.com>
2019-09-05 23:35:07 -0500

    tabs exercise complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Messages from './Messages';
 import TheDate from './state/TheDate';
 import Counter from './state/Counter';
 import Accordion from './state-drills/Accordion';
+import Tabs from './state/Tabs';
 
 // make 2 tooltips here and another inside the App directly
 const firstTooltip = (
@@ -34,11 +35,21 @@ const sections = [
 },
 ]
 
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
 
 function App() {
   return (
     <main className='App'>
       <h1>Name</h1>
+      <Tabs tabs={tabsProp} />
       <Accordion sections={sections} />
       {/* <TheDate />
       <Counter />
